4e8816d246e46e6f88f0783c2529d46469560716
ChyunKim <co1995@naver.com>
2024-02-12 16:00:42 +0900

    feat: footer set

--- components/common/CommonFooter.tsx
+++ components/common/CommonFooter.tsx
@@ -1,7 +1,10 @@
 function CommonFooter() {
   return (
-    <div className="w-full h-[40px] bg-gray-100 fixed bottom-0 left-0 flex items-center justify-end p-4">
-      <div>사방팔방</div>
+    <div className="w-full h-[40px] bg-gray-100 fixed bottom-0 left-0 flex items-center justify-end p-4 text-[12px] gap-1">
+      <div className="text-gray-500">사방팔방은 국내 여행 플랫폼입니다.</div>
+      <div className="text-gray-800 cursor-pointer font-[600]">
+        개인정보처리방침
+      </div>
     </div>
   );
 }
